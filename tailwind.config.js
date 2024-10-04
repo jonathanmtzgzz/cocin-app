8e09cc38f527321e1c12ee1f240256f1912cfa84
sofiaramirez <sofiaa.ramirez016@gmail.com>
2024-10-04 10:36:03 -0600

    Se crean estilos personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FA9E31", //naranja claro
+        secondary: "#FF6200", //naranja obscuro
+        textColor: {
+          heading: "#0A2533",
+          body: "#748189",
+        },
+        buttons: {
+          primary: {
+            //azulado
+            default: "#042628",
+            hover: "#031f21",
+          },
+          secondary: {
+            //gris
+            default: "#999999",
+            hover: "#5E5953",
+          },
+          tertiary: {
+            //naranja
+            default: "#FA9E31",
+            hover: "#fa941b",
+          },
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
