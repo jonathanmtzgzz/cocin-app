d36208731beb5d2dab70a4d51547297756c4c46b
sofiaramirez <sofiaa.ramirez016@gmail.com>
2024-10-05 08:22:58 -0600

    Se modifican y agregan  estilos de la vista de bienvenida

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
 const WelcomeScreen = ({ navigation }) => {
   return (
-    <View className="flex-1 justify-end bg-orange-500">
-      <View className="items-center mb-20">
-        <Text className="mb-10 mx-6 text-3xl text-white text-center">
+    <View className="flex-1 relative">
+      <Image
+        source={require("../../assets/splash-screen.png")}
+        style={{ width: "100%", height: "100%", resizeMode: "cover" }}
+      />
+      <View className="absolute bottom-0 left-0 right-0 p-4 items-center mb-20">
+        <Text className="mb-10 mx-6 text-2xl text-white text-center font-bold">
           Crea y organiza de una mejor manera tus recetas
         </Text>
         <TouchableOpacity
-          className="rounded-2xl bg-green-950 p-2 w-3/4"
+          className="rounded-2xl bg-buttons-primary-default p-2 w-full"
           onPress={() => navigation.navigate("Auth")}
         >
-          <Text className="text-white p-3 text-center">Login</Text>
+          <Text className="text-white p-3 text-center font-bold">Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+          <Text className="text-white p-3 text-center font-bold text-base">
+            Crear una nueva cuenta
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
