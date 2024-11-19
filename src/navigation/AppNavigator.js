feb2c930e21e1662e1ecfc6ceaf08fa0bc627f16
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-11-18 19:06:25 -0600

    Eliminar importación del HomeNavigator en AppNavigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthNavigator from "./AuthNavigator";
-import HomeNavigator from "./HomeNavigator";
+// import HomeNavigator from "./HomeNavigator";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import RecipeDetailScreen from "../screens/RecipeDetailScreen";
 import HomeScreen from "../screens/HomeScreen";
